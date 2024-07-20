a34b8ffddf7c7421294ca0c987e113c47678bf9d
Nourkhallaf <nkhallaf94@gmail.com>
2024-07-21 01:34:15 +0300

    Remove unwanted folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { UsersListComponent } from './pages/users-list/users-list.component';
-import { UserDetailsComponent } from './pages/user-details/user-details.component';
 import { CreateUserComponent } from './pages/create-user/create-user.component'
-import { UpdateUserComponent } from './pages/update-user/update-user.component';
-import { DeleteUserComponent } from './pages/delete-user/delete-user.component';
 import { AuthGuard } from './service/auth.guard';
 import { NoAuthGuard } from './service/noauth.guard';
 
@@ -14,12 +11,9 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, canActivate: [NoAuthGuard] },
   { path: 'users', component: UsersListComponent, canActivate: [AuthGuard] },
-  { path: 'user/:id', component: UserDetailsComponent, canActivate: [AuthGuard] },
   { path: 'create-user', component: CreateUserComponent, canActivate: [AuthGuard] },
-  { path: 'update-user/:id', component: UpdateUserComponent, canActivate: [AuthGuard] },
-  { path: 'delete-user/:id', component: DeleteUserComponent, canActivate: [AuthGuard] }
-];
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { UsersListComponent } from './pages/users-list/users-list.component';
-import { UserDetailsComponent } from './pages/user-details/user-details.component';
 import { CreateUserComponent } from './pages/create-user/create-user.component';
-import { UpdateUserComponent } from './pages/update-user/update-user.component';
 import { DeleteUserComponent } from './pages/delete-user/delete-user.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -19,9 +17,7 @@ import { LocalStorageService } from './service/localStorage.service';
     AppComponent,
     LoginComponent,
     UsersListComponent,
-    UserDetailsComponent,
     CreateUserComponent,
-    UpdateUserComponent,
     DeleteUserComponent
   ],
   imports: [
