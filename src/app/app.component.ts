e18bbb91508728fd3d3ab2e8343e82a5f27fc427
Nourkhallaf <nkhallaf94@gmail.com>
2024-07-20 23:36:48 +0300

    handle the login and logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from './service/localStorage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'user-lists';
+  isAuthurized: boolean = false;
+
+  constructor(private localStorageService: LocalStorageService, private router:Router){
+    if (localStorageService.getAuthorization()) {
+      console.log("User is authenticated")
+      router.navigate(['']);
+      this.isAuthurized = true;
+    } else {
+      console.log("User is not authenticated")
+      router.navigate(['login']);
+      this.isAuthurized = false;
+    }
+  }
+  logout() {
+
+    this.localStorageService.removeAuthorization();
+    this.router.navigate(['']);
+  }
 }
